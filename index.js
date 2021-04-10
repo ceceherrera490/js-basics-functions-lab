b7b89abeaf1f2a5118cfefeb381e2cdd9a5decab
Crystal <ceceherrera490@gmail.com>
2021-04-10 01:00:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  const pickUp = 42;
+  if (blocks > pickUp) {
+    return blocks - pickUp;
+  } else {
+    return pickUp - blocks;
+  }
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const startB = distanceFromHqInFeet(startBlock);
+  const endB = distanceFromHqInFeet(endBlock);
+  return endB - startB;
+}
+
+function calculatesFarePrice(start, destination) {
+  let travel = distanceTravelledInFeet(start, destination);
+  let freeSmaple = 0;
+  if (travel <= 400) {
+    return freeSmaple;
+  } else if (travel >= 400 && travel <= 2000) {
+    return (travel - 400) * 0.02;
+  } else if (travel > 2000 && travel <= 2500) {
+    return (travel = 25);
+  } else {
+    return `cannot travel that far`;
+  }
+}
